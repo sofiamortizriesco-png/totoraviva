bf9c543ea90d1e4f243fa4221cc3d973d78a5701
sofiamortizriesco-png <sofiam.ortizriesco@gmail.com>
2025-12-02 17:53:26 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-//Magic Tab JS
-  $('.magic-tab_link').on('click', function() {
-    $('.magic-pane').removeClass('current');
-    $('.magic-tab').removeClass('current');
-    $(this).closest('.magic-pane').toggleClass('current');
-    $(this).closest('.magic-tab').toggleClass('current');
-    $(this).closest('.magic-tab').siblings('.magic-pane').toggleClass('current');
-  });
